4d287f914940f84d9597812bfd0820aa124d292c
sansepro <sanse.pro@yandex.ru>
2026-05-16 15:04:35 +0500

    add i18n russian

--- application/i18n/messages.ts
+++ application/i18n/messages.ts
@@ -1,11 +1,13 @@
 import en, { type Messages } from './locales/en';
 import zhCN from './locales/zh-CN';
+import ru from './locales/ru';
 
 // Keep keys stable; add new locales by adding another import and map entry.
 export { type Messages };
 
 export const MESSAGES_BY_LOCALE: Record<string, Messages> = {
   en,
+  ru,
   'zh-CN': zhCN,
 };
 
